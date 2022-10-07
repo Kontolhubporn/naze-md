9b5001d8ef1a85e6fa018dc6fa1a971b1a628b95
Kontolhubporn <109457729+Kontolhubporn@users.noreply.github.com>
2022-10-08 02:45:11 +0700

    Update config.js

--- config.js
+++ config.js
@@ -19,11 +19,11 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6285875158363']
-global.ownernomer = "6285875158363"
-global.premium = ['6285875158363']
-global.packname = 'Sticker  Naze'
-global.author = 'Naze'
+global.owner = ['6285770356017']
+global.ownernomer = "6285770356017"
+global.premium = ['6285770356017']
+global.packname = 'Sticker  Kyy'
+global.author = 'Kyy'
 global.sessionName = 'nazedev'  //jangan diganti bro nanti error
 global.prefa = ['','!','.','#','&']
 global.sp = ''
